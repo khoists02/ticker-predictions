f117ff20e64bc11b6ec2238d854234370ff7e302
Khoi LE <lekhoi@Khois-MacBook-Pro.local>
2023-07-19 16:26:35 +0700

    add

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
-from flask_restful import Api
+from flask_restful import Api, abort
 from resources.api.main import Main
+from webargs.flaskparser import parser
 
 
 app = Flask(__name__)
@@ -8,5 +9,11 @@ api = Api(app)
 
 api.add_resource(Main, '/api/v1/main')
 
+
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    abort(error_status_code, errors=err.messages)
+
+
 if __name__ == '__main__':
-  app.run()
+    app.run()
--- app/resources/api/main/__init__.py
+++ app/resources/api/main/__init__.py
@@ -1,14 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, abort
 from flask import jsonify
 import numpy as np
 import pandas as pd
 import random
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, Bidirectional
-from keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.python.keras.layers import LSTM, Dense, Dropout
+from tensorflow.python.keras.callbacks import ModelCheckpoint, TensorBoard
 import time
 from resources.helpers import Helpers
+from webargs import fields, validate
+from webargs.flaskparser import use_kwargs, parser
+
 
 class Main(Resource):
     def __init__(self):
@@ -17,7 +19,7 @@ class Main(Resource):
         np.random.seed(314)
         tf.random.set_seed(314)
         random.seed(314)
-        
+
         # Window size or the sequence length
         self.N_STEPS = 50
         # Lookup step, 1 is the next day
@@ -37,7 +39,7 @@ class Main(Resource):
         self.FEATURE_COLUMNS = ["adjclose", "volume", "open", "high", "low"]
         # date now
         self.date_now = time.strftime("%Y-%m-%d")
-        ### model parameters
+        # model parameters
         self.N_LAYERS = 2
         # LSTM cell
         self.CELL = LSTM
@@ -47,7 +49,7 @@ class Main(Resource):
         self.DROPOUT = 0.4
         # whether to use bidirectional RNNs
         self.BIDIRECTIONAL = False
-        ### training parameters
+        # training parameters
         # mean absolute error loss
         # LOSS = "mae"
         # huber loss
@@ -58,11 +60,45 @@ class Main(Resource):
         # Amazon stock market
         self.ticker = "AMZN"
 
+    args = {
+        'ticker': fields.Str(
+            required=True,
+            default='AMZN'
+        ),
+        'steps': fields.Int(
+            required=False,
+            default=50
+        ),
+        'scale': fields.Bool(
+            required=False,
+            default=False
+        ),
+        'split_by_date': fields.Bool(
+            required=False,
+            default=False
+        ),
+        'cols': fields.Str(
+            required=True
+        )
+    }
 
-    def get(self):
-        data = self.helper.load_stock_data(self.ticker, self.N_STEPS, scale=self.SCALE, split_by_date=self.SPLIT_BY_DATE,
-                shuffle=self.SHUFFLE, lookup_step=self.LOOKUP_STEP, test_size=self.TEST_SIZE,
-                feature_columns=self.FEATURE_COLUMNS)
+    @use_kwargs(args, location="query")
+    def get(self, ticker, steps, scale, split_by_date, cols: str):
+
+        feature_cols = []
+        if len(cols):
+            feature_cols = cols.split(',')
+        print(ticker, steps, scale, split_by_date, feature_cols)
+        data = self.helper.load_stock_data(
+            ticker=ticker,
+            n_steps=steps,
+            scale=self.SCALE,
+            split_by_date=self.SPLIT_BY_DATE,
+            shuffle=self.SHUFFLE,
+            lookup_step=self.LOOKUP_STEP,
+            test_size=self.TEST_SIZE,
+            feature_columns=self.FEATURE_COLUMNS
+        )
         model = self.create_model(
             sequence_length=self.N_STEPS,
             n_features=len(self.FEATURE_COLUMNS),
@@ -74,55 +110,72 @@ class Main(Resource):
             optimizer=self.OPTIMIZER,
             bidirectional=self.BIDIRECTIONAL
         )
-        history = model.fit(data["X_train"], data["y_train"],
-                    batch_size=self.BATCH_SIZE,
-                    epochs=self.EPOCHS,
-                    validation_data=(data["X_test"], data["y_test"]),
-                    verbose=1)
+        history = model.fit(
+            data["X_train"],
+            data["y_train"],
+            batch_size=self.BATCH_SIZE,
+            epochs=self.EPOCHS,
+            validation_data=(data["X_test"], data["y_test"]),
+            verbose=1
+        )
+        # convert the history.history dict to a pandas DataFrame:
+        hist_df = pd.DataFrame(history.history)
+        # save to json:
+        hist_json_file = 'history.json'
+        with open(hist_json_file, mode='w') as f:
+            hist_df.to_json(f)
+
         future_price = self.predict(model, data)
-        print(f"Future price after {self.LOOKUP_STEP} days is {future_price:.2f}$")
+        print(
+            f"Future price after {self.LOOKUP_STEP} days is {future_price:.2f}$")
         return jsonify({'price': f"Future price after {self.LOOKUP_STEP} days is {future_price:.2f}$"})
-    
+
     def create_model(self, sequence_length, n_features, units=256, cell=LSTM, n_layers=2, dropout=0.3,
-                loss="mean_absolute_error", optimizer="rmsprop", bidirectional=False):
-      model = Sequential()
-      print("access_function")
-      for i in range(n_layers):
-          if i == 0:
-              # first layer
-              if bidirectional:
-                  model.add(Bidirectional(cell(units, return_sequences=True), batch_input_shape=(None, sequence_length, n_features)))
-              else:
-                  model.add(cell(units, return_sequences=True, batch_input_shape=(None, sequence_length, n_features)))
-          elif i == n_layers - 1:
-              # last layer
-              if bidirectional:
-                  model.add(Bidirectional(cell(units, return_sequences=False)))
-              else:
-                  model.add(cell(units, return_sequences=False))
-          else:
-              # hidden layers
-              if bidirectional:
-                  model.add(Bidirectional(cell(units, return_sequences=True)))
-              else:
-                  model.add(cell(units, return_sequences=True))
-          # add dropout after each layer
-          model.add(Dropout(dropout))
-      model.add(Dense(1, activation="linear"))
-      model.compile(loss=loss, metrics=["mean_absolute_error"], optimizer=optimizer)
-      return model
-    
-    def predict(self,model, data):
-      # retrieve the last sequence from data
-      last_sequence = data["last_sequence"][-self.N_STEPS:]
-      # expand dimension
-      last_sequence = np.expand_dims(last_sequence, axis=0)
-      # get the prediction (scaled from 0 to 1)
-      prediction = model.predict(last_sequence)
-      # get the price (by inverting the scaling)
-      if self.SCALE:
-          predicted_price = data["column_scaler"]["adjclose"].inverse_transform(prediction)[0][0]
-      else:
-          predicted_price = prediction[0][0]
-      return predicted_price
-    
+                     loss="mean_absolute_error", optimizer="rmsprop", bidirectional=False):
+        model = tf.keras.models.Sequential()
+        print("access_function")
+
+        for i in range(n_layers):
+            if i == 0:
+                # first layer
+                if bidirectional:
+                    model.add(tf.keras.layers.Bidirectional(cell(units, return_sequences=True), batch_input_shape=(
+                        None, sequence_length, n_features)))
+                else:
+                    model.add(cell(units, return_sequences=True, batch_input_shape=(
+                        None, sequence_length, n_features)))
+            elif i == n_layers - 1:
+                # last layer
+                if bidirectional:
+                    model.add(tf.keras.layers.Bidirectional(
+                        cell(units, return_sequences=False)))
+                else:
+                    model.add(cell(units, return_sequences=False))
+            else:
+                # hidden layers
+                if bidirectional:
+                    model.add(tf.keras.layers.Bidirectional(
+                        cell(units, return_sequences=True)))
+                else:
+                    model.add(cell(units, return_sequences=True))
+            # add dropout after each layer
+            model.add(Dropout(dropout))
+        model.add(Dense(1, activation="linear"))
+        model.compile(loss=loss, metrics=[
+                      "mean_absolute_error"], optimizer=optimizer)
+        return model
+
+    def predict(self, model, data):
+        # retrieve the last sequence from data
+        last_sequence = data["last_sequence"][-self.N_STEPS:]
+        # expand dimension
+        last_sequence = np.expand_dims(last_sequence, axis=0)
+        # get the prediction (scaled from 0 to 1)
+        prediction = model.predict(last_sequence)
+        # get the price (by inverting the scaling)
+        if self.SCALE:
+            predicted_price = data["column_scaler"]["adjclose"].inverse_transform(prediction)[
+                0][0]
+        else:
+            predicted_price = prediction[0][0]
+        return predicted_price
