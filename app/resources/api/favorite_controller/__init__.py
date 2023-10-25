04ba9e3759ae9d9435762fe29ebb1a2b53404b75
Khoi LE <khoi.kioto@gmail.com>
2023-10-25 08:37:40 +0700

    dasdas

--- app/resources/api/favorite_controller/__init__.py
+++ app/resources/api/favorite_controller/__init__.py
@@ -1,10 +1,9 @@
 from flask_restful import Resource
-from flask import abort
+from flask import abort, json
 from webargs import fields
 from webargs.flaskparser import use_kwargs, use_args
 from resources.models.favorite import Favorite, FavoriteQuery
 from resources.helpers import Helpers
-import json
 
 
 class FavoriteController(Resource):
@@ -22,6 +21,20 @@ class FavoriteController(Resource):
             required=True
         ),
     }
+
+    body = {
+        'ticker': fields.String(
+            required=True,
+        )
+    }
+
+    @use_args(body)
+    def post(self, body):
+        qr = FavoriteQuery()
+        qr.addNew(ticker=body["ticker"])
+        return {
+            'message': 'Create Ok !!!'
+        }, 201
 
     @use_kwargs(args, location='query')
     def get(self, ticker: str):
@@ -51,9 +64,8 @@ class FavoriteController(Resource):
             item['shortName'] = ticker
             item['industryDisp'] = "Computer"
             item['recommendationKey'] = None
-            item['previousClose'] = item['regularMarketPreviousClose']
+            item['previousClose'] = item['regularMarketPreviousClose'] if item['regularMarketPreviousClose'] is not None else 0
             rs.append(item)
-
         return {
             'content': rs
         }, 200
@@ -67,20 +79,6 @@ class FavoriteController(Resource):
         }, 200
 
 
-class FavoriteAdd(Resource):
-    def __init__(self):
-        self.helper = Helpers()
-
-    body = {
-        'ticker': fields.String(
-            required=True,
-        )
-    }
-
-    @use_args(body)
-    def post(self, body):
-        qr = FavoriteQuery()
-        qr.addNew(ticker=body["ticker"])
-        return {
-            'message': 'Create Ok !!!'
-        }, 201
+# class FavoriteAdd(Resource):
+#     def __init__(self):
+#         self.helper = Helpers()
